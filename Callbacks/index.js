b123e0eaa24602bb6a2b9bb132cb7536f07b94a6
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-01 15:53:27 +0530

    callback

--- Callbacks/index.js
+++ Callbacks/index.js
@@ -1,6 +1,20 @@
-let time = () =>{
-    console.log('Time is over');
+// let time = () =>{
+//     console.log('Time is over');
+// }
+// console.log('one')
+// setTimeout(time, 2000)
+// console.log('second')
+
+
+// callbacks 
+
+// any function arguments pass another function called callbacks
+
+function sum(a,b){
+    console.log(a+b)
 }
-console.log('one')
-setTimeout(time, 2000)
-console.log('second')
+
+let cal = (a,b,callback) =>{
+     callback(a,b)
+}
+cal(2,30,sum)
