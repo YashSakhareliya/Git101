9f2df52c1187aabb73c9a962af6de4164deffdb2
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-01-01 16:28:22 +0530

    Promis

--- Callbacks/index.js
+++ Callbacks/index.js
@@ -10,11 +10,30 @@
 
 // any function arguments pass another function called callbacks
 
-function sum(a,b){
-    console.log(a+b)
-}
+// function sum(a,b){
+//     console.log(a+b)
+// }
 
-let cal = (a,b,callback) =>{
-     callback(a,b)
-}
-cal(2,30,sum)
+// let cal = (a,b,callback) =>{
+//      callback(a,b)
+// }
+// cal(2,30,sum)
+
+
+// //  Callback hell
+// // -----> nesting callback
+
+// // promise
+
+// function getPromis(data , getNextData){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             console.log(data)
+//             resolve("success")
+//         },5000)
+//         if(getNextData){
+//             getNextData()
+//         }
+//     })
+    
+// }
